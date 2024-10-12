d6ad17e66bd8a179f98fc00e30e7859139d28fbd
sirajsoomro <sirajsoomro010@gmail.com>
2024-10-12 16:15:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// for (var a = 1; a <= 10 ; a++){
+//     console.log(a);  
+// }
+
+
+//  var       condition  increment / decrement   
+for(var i = 1; i < 10; i++){
+    console.log('haan', i); // 1
+}
+
+// var siraj = 20;
+
